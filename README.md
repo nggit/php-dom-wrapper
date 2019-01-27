2895853f6f2b46ece87ae5a73015130568b901fd
nggit <contact@anggit.com>
2019-01-27 16:24:05 +0659

    upgrade

--- README.md
+++ README.md
@@ -1,2 +1,121 @@
-# MyDOM
-PHP template engine using DOM
+<!-- <?php /* -->
+# PHP DOM Template
+**Tip:** Rename this `md` file to `php` to test it on your web server.
+<!-- */ ?> -->
+
+    <?php
+
+    require 'src/HTML.php';
+
+    $str = '<!DOCTYPE html>
+                <head>
+                    <title>Home</title>
+                    <style>
+                    body { border: 1px solid #c0c0c0; }
+                    .nav, .item { border-top: 1px solid #c0c0c0; }
+                    .center { text-align: center; }
+                    .small { font-size: small; color: red; }
+                    </style>
+                </head>
+                <body>
+                    <div class="content header">
+                        <h1>Home</h1>
+                        <p>Default Page</p>
+                    </div>
+                    <div class="content main">
+                        <div class="item" id="item">
+                            <h2 id="title">Item Title</h2>
+                            <p class="small" id="description">Item Description</a>
+                            <p id="content">Item Content</p>
+                        </div>
+                    </div>
+                    <div class="content nav">
+                        <div class="center">
+                            <a id="navpage" href="?">Next Page</a>
+                        <div/>
+                    </div>
+                </body>
+            </html>';
+
+### Load the HTML
+    // This can be an HTML string or a local path of the HTML file, e.g. /path/template.html
+    $html = new Nggit\PHPDOMTemplate\HTML($str);
+
+    switch ($_SERVER['QUERY_STRING']) {
+        default:
+## Templating
+### Set attribute with attr(), using find() and the #navpage (id="navpage") as a selector
+            $html->find('#navpage')->attr('href', '?page-1');
+### Render the HTML
+            // Stop modifying and just display the results with render():
+            $html->render();
+            // If you need to store the results to a variable:
+            // $var = $html->output();
+            break;
+        case 'page-1':
+### Put data to #item (id="item"), data will be assigned to child elements that have ids related to array keys
+            $data                   = array();
+            $data[0]['title']       = 'The Flash';
+            /* null or undefined will cause the removal of the corresponding elements if they only have 3 children or less
+            $data[0]['description'] = null;
+            $data[0]['content']     = null;
+            */
+            $data[1]['title']       = 'Barry Allen';
+            $data[1]['description'] = 'Flash, Meta Human (Speedster)';
+            $data[1]['content']     = "My name is Barry Allen and I'm the fastest man alive.";
+            $data[2]['title']       = 'Clifford Devoe';
+            $data[2]['description'] = 'The Thinker, Meta Human';
+            $data[2]['content']     = "You could gather every genius on every planet, and you still couldn't out-think me.<br />
+                                       You may be the <i>fastest man</i> alive, Allen. I'm the <i>fastest mind</i>.";
+            $data[3]['title']       = 'Eobard Thawne';
+            $data[3]['description'] = 'Reverse Flash, Meta Human (Speedster)';
+            $data[3]['content']     = 'I told you this before. I am always one step ahead, Flash.';
+            /* Begin put data with find() */
+            $html->find('#item');
+            foreach ($data as $item) {
+                $html->put($item);
+            }
+            $html->clean();
+            /* End put data with clean() */
+### Set inner element with text()
+            $html->find('title')->text('Page 1');
+### Set inner element with html()
+            $html->find('.header')->html('<div class="content"><h1>Page 1</h1><p>You are viewing Page 1</p></div>');
+            $html->find('#navpage')->attr('href', '?page-2');
+            $html->render();
+            break;
+        case 'page-2':
+### Set outer element with replace()
+            $html->find('.main')->replace('<div class="content"><h2>Nothing Here</h2></div>');
+### Remove an element with find() and remove()
+            $html->find('.header')->remove();
+            $html->find('#navpage')->attr('href', '?page-3');
+            $html->render();
+            break;
+        case 'page-3':
+### Remove elements with prepare() and remove()
+            $html->prepare('style', '.header', '#content', '#description')->remove();
+            // Or call remove() later individually:
+            // $html->remove();
+### Prepending and Appending elements
+            $html->find('.main')->prepend('<div class="item"><h3>Before Item</h3></div>')->append('<div class="item"><h3>After Item</h3></div>');
+            $html->find('#navpage')->attr('href', '?page-4');
+            $html->render();
+            break;
+        case 'page-4':
+## Extraction
+### Get the attribute value
+            // Find the second p (index 1):
+            $class = $html->find('p', 1)->attr('class');
+### Get the inner text
+            $header = $html->find('.header')->text();
+### Get the inner HTML
+            $nav = $html->find('.nav')->html();
+            echo '$class contains: ' . $class . '<br />$header contains: ' . $header . '<br />$nav contains: ' . $nav;
+            break;
+    }
+    exit;
+
+    ?>
+
+And more to explore...
--- src/HTML.php
+++ src/HTML.php
@@ -4,32 +4,37 @@
 # PHP template engine using DOM
 # (c) 20161228 nggit
 
-class MyDOM {
+namespace Nggit\PHPDOMTemplate;
 
+class HTML
+{
     protected $dom, $xpath, $node = array(), $element = array(null, null, null), $prepare = array();
 
-    public function __construct($str) {
-        $this->dom = new DOMDocument();
+    public function __construct($str)
+    {
+        $this->dom = new \DOMDocument();
         libxml_use_internal_errors(true);
         if (file_exists($str)) {
             $this->dom->loadHTMLFile($str);
-        }
-        else {
+        } else {
             $this->dom->loadHTML($str);
         }
         libxml_clear_errors();
-        $this->xpath = new DOMXPath($this->dom);
+        $this->xpath = new \DOMXPath($this->dom);
     }
 
-    protected function getElementById($id) { // safe getElementById()
+    protected function getElementById($id) // safe getElementById()
+    {
         return $this->xpath->query("//*[@id='$id']")->item(0);
     }
 
-    protected function getElementsByClassName($class) {
+    protected function getElementsByClassName($class)
+    {
         return $this->xpath->query("//*[contains(concat(' ', @class, ' '), ' $class ')]");
     }
 
-    public function find($item, $n = 0) {
+    public function find($item, $n = 0) // selector
+    {
         switch ($item[0]) {
             case '#':
                 isset($this->node[$item][$n]) or $this->node[$item][$n] = $this->getElementById(ltrim($item, '#'));
@@ -46,14 +51,14 @@ class MyDOM {
         return $this;
     }
 
-    public function replace($content) {
+    public function replace($content = null) // outer
+    {
         if ($this->element[1]) {
             if (is_object($content)) {
                 $fragment = $content;
-            }
-            else{
+            } else {
                 $fragment = $this->dom->createDocumentFragment();
-                $fragment->appendXML('<![CDATA['.$content.']]>');
+                $fragment->appendXML('<![CDATA[' . $content . ']]>');
             }
             $element = $this->element[1];
             $element->parentNode->replaceChild($fragment, $element);
@@ -62,14 +67,37 @@ class MyDOM {
         }
     }
 
-    public function prepend($content) {
+    public function html($content = null) // inner
+    {
+        if ($this->element[1]) {
+            if (is_null($content)) {
+                $str   = $this->get($this->element[1]);
+                $start = strpos($str, '>') + 1;
+                return substr($str, $start, strrpos($str, '<') - $start);
+            }
+            if (is_object($content)) {
+                $fragment = $content;
+            } else {
+                $fragment = $this->dom->createDocumentFragment();
+                $fragment->appendXML('<![CDATA[' . $content . ']]>');
+            }
+            $element = $this->element[1];
+            $node    = $element->cloneNode();
+            $node->appendChild($fragment);
+            $element->parentNode->replaceChild($node, $element);
+            unset($this->node[$this->element[0]][$this->element[2]]);
+            $this->element = array(null, null, null);
+        }
+    }
+
+    public function prepend($content)
+    {
         if ($this->element[1]) {
             if (is_object($content)) {
                 $fragment = $content;
-            }
-            else {
+            } else {
                 $fragment = $this->dom->createDocumentFragment();
-                $fragment->appendXML('<![CDATA['.$content.']]>');
+                $fragment->appendXML('<![CDATA[' . $content . ']]>');
             }
             $element = $this->element[1];
             $element->insertBefore($fragment, $element->firstChild);
@@ -78,14 +106,14 @@ class MyDOM {
         return $this;
     }
 
-    public function append($content) {
+    public function append($content)
+    {
         if ($this->element[1]) {
             if (is_object($content)) {
                 $fragment = $content;
-            }
-            else {
+            } else {
                 $fragment = $this->dom->createDocumentFragment();
-                $fragment->appendXML('<![CDATA['.$content.']]>');
+                $fragment->appendXML('<![CDATA[' . $content . ']]>');
             }
             $element = $this->element[1];
             $element->appendChild($fragment);
@@ -94,14 +122,14 @@ class MyDOM {
         return $this;
     }
 
-    public function before($content) {
+    public function before($content)
+    {
         if ($this->element[1]) {
             if (is_object($content)) {
                 $fragment = $content;
-            }
-            else {
+            } else {
                 $fragment = $this->dom->createDocumentFragment();
-                $fragment->appendXML('<![CDATA['.$content.']]>');
+                $fragment->appendXML('<![CDATA[' . $content . ']]>');
             }
             $element = $this->element[1];
             $element->parentNode->insertBefore($fragment, $element);
@@ -110,14 +138,14 @@ class MyDOM {
         return $this;
     }
 
-    public function after($content) {
+    public function after($content)
+    {
         if ($this->element[1]) {
             if (is_object($content)) {
                 $fragment = $content;
-            }
-            else {
+            } else {
                 $fragment = $this->dom->createDocumentFragment();
-                $fragment->appendXML('<![CDATA['.$content.']]>');
+                $fragment->appendXML('<![CDATA[' . $content . ']]>');
             }
             $element = $this->element[1];
             $element->parentNode->insertBefore($fragment, $element->nextSibling);
@@ -126,8 +154,12 @@ class MyDOM {
         return $this;
     }
 
-    public function setText($content) {
+    public function text($content = null)
+    {
         if ($this->element[1]) {
+            if (is_null($content)) {
+                return $this->element[1]->textContent;
+            }
             $element = $this->element[1];
             $text    = $this->dom->createTextNode($content);
             $node    = $element->cloneNode();
@@ -138,8 +170,12 @@ class MyDOM {
         }
     }
 
-    public function setAttr($name, $value = null) {
+    public function attr($name, $value = null)
+    {
         if ($this->element[1]) {
+            if (is_null($value)) {
+                return $this->element[1]->getAttribute($name);
+            }
             $element = $this->element[1];
             $element->setAttribute($name, $value);
             $this->element[0] = null;
@@ -147,7 +183,8 @@ class MyDOM {
         return $this;
     }
 
-    public function removeAttr($name) {
+    public function removeAttr($name)
+    {
         if ($this->element[1]) {
             $element = $this->element[1];
             $element->removeAttribute($name);
@@ -156,13 +193,15 @@ class MyDOM {
         return $this;
     }
 
-    public function prepare($items) {
+    public function prepare($items)
+    {
         is_array($items) or $items = func_get_args();
         $this->prepare = array_merge($this->prepare, $items);
         return $this;
     }
 
-    public function remove($item = null) {
+    public function remove($item = null)
+    {
         switch (true) {
             case $item:
                 $items = func_get_args();
@@ -182,8 +221,7 @@ class MyDOM {
                             $element->parentNode->removeChild($element);
                         }
                         unset($this->node[$prepare]);
-                    }
-                    else {
+                    } else {
                         switch ($prepare[0]) {
                             case '#':
                                 $element = $this->getElementById(ltrim($prepare, '#'));
@@ -216,7 +254,8 @@ class MyDOM {
         }
     }
 
-    public function dup($n = null) {
+    public function dup($n = null)
+    {
         if ($this->element[1]) {
             if ($n > 0) {
                 for ($j = 1; $j <= $n; $j++) {
@@ -227,16 +266,15 @@ class MyDOM {
                         $i--;
                         $element = $elements->item($i);
                         if ($element->hasAttribute('id')) {
-                            $element->setAttribute('id', $element->getAttribute('id').$j);
+                            $element->setAttribute('id', $element->getAttribute('id') . $j);
                         }
                     }
                     if ($dup->hasAttribute('id')) {
-                        $dup->setAttribute('id', $dup->getAttribute('id').$j);
+                        $dup->setAttribute('id', $dup->getAttribute('id') . $j);
                     }
                     $this->element[1]->parentNode->insertBefore($dup, $this->element[1]);
                 }
-            }
-            else {
+            } else {
                 $dup      = $this->element[1]->cloneNode(true);
                 $elements = $dup->getElementsByTagName('*');
                 $i        = $elements->length;
@@ -244,18 +282,19 @@ class MyDOM {
                     $i--;
                     $element = $elements->item($i);
                     if ($element->hasAttribute('id')) {
-                        $element->setAttribute('id', $element->getAttribute('id').'1');
+                        $element->setAttribute('id', $element->getAttribute('id') . '1');
                     }
                 }
                 if ($dup->hasAttribute('id')) {
-                    $dup->setAttribute('id', $dup->getAttribute('id').'1');
+                    $dup->setAttribute('id', $dup->getAttribute('id') . '1');
                 }
                 return $dup;
             }
         }
     }
 
-    public function put($data) {
+    public function put($data)
+    {
         if ($this->element[1]) {
             static $n = 0;
             $n++;
@@ -271,11 +310,10 @@ class MyDOM {
                         if ($element->childNodes->length <= 3) {
                             $element->parentNode->removeChild($element);
                         }
-                    }
-                    else {
-                        $element->setAttribute('id', $attribute.$n);
+                    } else {
+                        $element->setAttribute('id', $attribute . $n);
                         $fragment = $this->dom->createDocumentFragment();
-                        $fragment->appendXML('<![CDATA['.$data[$attribute].']]>');
+                        $fragment->appendXML('<![CDATA[' . $data[$attribute] . ']]>');
                         $node = $element->cloneNode();
                         $node->appendChild($fragment);
                         $element->parentNode->replaceChild($node, $element);
@@ -283,18 +321,29 @@ class MyDOM {
                 }
             }
             if ($dup->hasAttribute('id')) {
-                $dup->setAttribute('id', $dup->getAttribute('id').$n);
+                $dup->setAttribute('id', $dup->getAttribute('id') . $n);
             }
             $this->element[1]->parentNode->insertBefore($dup, $this->element[1]);
         }
     }
 
-    public function output() {
+    public function clean()
+    {
+        $this->remove();
+    }
+
+    public function output()
+    {
         return $this->dom->saveHTML();
     }
 
-    public function render() {
+    public function render()
+    {
         echo $this->dom->saveHTML();
     }
 
+    protected function get($element)
+    {
+        return version_compare(PHP_VERSION, '5.3.6', '>=') ? $this->dom->saveHTML($element) : $this->dom->saveXML($element);
+    }
 }
